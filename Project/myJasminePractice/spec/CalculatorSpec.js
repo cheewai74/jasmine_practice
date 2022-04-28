d276814de19d9eba2e6f7e03057128adbcb68d28
moses <academicsnotes@gmail.com>
2022-04-28 20:19:59 +0800

    Commit fixes

--- Project/myJasminePractice/spec/CalculatorSpec.js
+++ Project/myJasminePractice/spec/CalculatorSpec.js
@@ -1,5 +1,9 @@
 describe("calculator", function(){
 
+    beforeEach(function() {
+        Calculator.currentVal = 0;
+      });
+
     //test case: 1
     it("Should retain the current value of all time", function(){
         expect(Calculator.currentVal).toBeDefined();
@@ -14,7 +18,7 @@ describe("calculator", function(){
 
     //test case: 3
     it("Should add any number of numbers", function(){
-        expect(Calculator.addAny(1,2,3)).toEqual(16);
+        expect(Calculator.addAny(1,2,3)).toEqual(6);
     });
 
 })
