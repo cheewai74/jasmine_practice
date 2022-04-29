d7d5a68011d8d6a7585bd062ee7abcaf9eb68182
moses <academicsnotes@gmail.com>
2022-04-29 21:06:14 +0800

    Commit scripts

--- Project/myJasminePractice/spec/CalculatorSpec.js
+++ Project/myJasminePractice/spec/CalculatorSpec.js
@@ -1,19 +1,28 @@
 describe("calculator", function(){
 
     beforeEach(function() {
+        console.log("beforeEach is triggered");
         Calculator.currentVal = 0;
-      });
+    });
+
+    afterEach(function(){
+        console.log("afterEach is triggered");
+     });
 
     //test case: 1
     it("Should retain the current value of all time", function(){
-        expect(Calculator.currentVal).toBeDefined();
+        expect(Calculator.currentVal).toBeDefined(); // Null Check
         expect(Calculator.currentVal).toEqual(0);
+        expect(Calculator.currentVal).toBeLessThan(5);
+        expect(Calculator.currentVal).not.toEqual(5);
     });
 
     //test case: 2
     it("Should add numbers", function(){
        expect(Calculator.add(5)).toEqual(5);
        expect(Calculator.add(5)).toEqual(10);
+       expect(Calculator.add(5.2)).toBeCloseTo(15.15,1);
+       expect(Calculator.currentVal).toBeGreaterThan(5);
     });
 
     //test case: 3
@@ -21,4 +30,18 @@ describe("calculator", function(){
         expect(Calculator.addAny(1,2,3)).toEqual(6);
     });
 
-})
+    //test case: 4
+    it("Should be a number", function(){
+        // Not a number check
+        expect(Calculator.add()).toBeNaN();
+    });
+
+    var throwMeAnError = ()=>{
+        throw new Error();
+    };
+
+    //test case: 5
+    it("Hey this will throw an error", ()=>{
+        expect(throwMeAnError).toThrow();
+    });
+});
--- Project/myJasminePractice/spec/helloworldSpec.js
+++ Project/myJasminePractice/spec/helloworldSpec.js
@@ -2,5 +2,11 @@ describe("helloWorld", () => {
     it("returns hello world", () => {
       var actual = helloWorld();
       expect(actual).toBe("Hello world!");
+      expect(actual).toMatch("Hello world!");
+      expect(actual).not.toMatch("Hello earth!");
+      expect(actual).not.toBe("Hello earth!");
+      expect(actual).toContain("world!");
     });
+
+
   })
